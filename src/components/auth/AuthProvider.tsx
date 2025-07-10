dc72a5c5c684d7c89e648c426bde2a5f1b9a072b
Tunji Bankole <shubbywax@gmail.com>
2025-07-10 14:31:50 +0100

    code the authprovider

---
+++ src/components/auth/AuthProvider.tsx
@@ -0,0 +1,37 @@
+import { createContext, useEffect, useState } from "react";
+import { onAuthStateChanged, User } from "firebase/auth";
+import {  } from "../../services/firebase/config";
+
+interface AuthContextType {
+
+    currentUser: User | null
+    loading: boolean 
+}
+
+export const AuthContext = createContext<AuthContextType>({
+
+    currentUser: null,
+    loading: true
+})
+
+export const AuthProvider: React.FC<{ children: React.ReactNode}> = ({ children }) => {
+
+    const [currentUser, setCurrentUser] = useState<User | null > (null)
+
+    const [loading, setLoading] = useState(true)
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth,(user) => {
+            setCurrentUser(user)
+            setLoading(false)
+        })
+        return unsubscribe
+    }, [])
+
+    return (
+        <AuthContext.Provider value={{ currentUser, loading}} >
+            {!loading && children}
+        </AuthContext.Provider>
+    )
+    
+}
